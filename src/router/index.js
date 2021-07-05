3309881310a248d82b5e7d3d36b8ea8981ed2d6e
Raphael Astrow <raphaelastrow@zoho.com>
2021-07-05 15:11:30 -0400

    Named Routes and Params

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Brazil from "../views/Brazil"
-import Hawaii from "../views/Hawaii"
-import Jamaica from "../views/Jamaica"
-import Panama from "../views/Panama"
 
 Vue.use(VueRouter);
 
@@ -26,26 +22,32 @@ const routes = [
   {
     path: "/brazil",
     name: "brazil",
-    component: Brazil
+    component: () => import(/* webpackChunkName: "brazil" */ "../views/Brazil")
   },
   {
     path: "/hawaii",
     name: "hawaii",
-    component: Hawaii
+    component: () => import(/* webpackChunkName: "hawaii" */ "../views/Hawaii")
   },
   {
     path: "/jamaica",
     name: "jamaica",
-    component: Jamaica
+    component: () => import(/* webpackChunkName: "jamaica" */ "../views/Jamaica")
   },
   {
     path: "/panama",
     name: "panama",
-    component: Panama
+    component: () => import(/* webpackChunkName: "panama" */ "../views/Panama")
   },
+  {
+    path: "/details",
+    name: "DestinationDetails",
+    component: () => import(/* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails")
+  }
 ];
 
 const router = new VueRouter({
+  linkExactActiveClass: "vue-school-active-class",
   routes,
 });
 
