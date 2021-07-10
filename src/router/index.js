0c645f428d0f28912d74718b8aae670cc591dd26
Raphael Astrow <raphaelastrow@zoho.com>
2021-07-10 12:15:25 -0400

    About to start advanced section.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "@/store"
 
 Vue.use(VueRouter);
 
@@ -12,10 +13,39 @@ const routes = [
     component: Home,
   },
   {
-    path: "/details/:slug",
+    path: "/destination/:slug",
     name: "DestinationDetails",
     props: true,
-    component: () => import(/* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails")
+    component: () => import(/* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails"),
+    children: [
+      {
+        path: ":experienceSlug",
+        name: "experienceDetails",
+        props: true,
+        component: () => import(/* webpackChunkName: "ExperienceDetails" */ "../views/ExperienceDetails")
+
+      }
+    ],
+    beforeEnter: (to, from, next) => {
+      const exists = store.destinations.find(
+        destination => destination.slug === to.params.slug
+      )
+      if (exists) {
+        next()
+      }
+      else {
+        next({name: "notFound"})
+      }
+    }
+  },
+  {
+    path: "/404",
+    alias: "*",
+    name: "notFound",
+    component: () => import(
+      /* webpackChunkName: "NotFound" */
+      "../views/NotFound"
+    )
   }
 ];
 
